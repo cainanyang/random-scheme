4d532bd92225917d0dfa0c10f117fe77e743c716
cainanyang <nanyang.cai@51fanli.com>
2013-08-12 19:57:42 +0800

    ch2 and ch3

---
+++ plai2/ch2/interp.rkt
@@ -0,0 +1,40 @@
+#lang plai-typed
+
+;; parsing is the act of turning an input character into
+;; a more structured, internal representation.
+
+; (read)
+
+; (define l '(+ 1 2))
+
+; convert s-expression to list
+; s-exp->list
+; first
+; s-exp->symbol
+; symbol->string
+
+(define-type ArithC
+  [numC (n : number)]
+  [plusC (l : ArithC) (r : ArithC)]
+  [multC (l : ArithC) (r : ArithC)])
+
+(define (parse [s : s-expression]) : ArithC
+  (cond
+    [(s-exp-number? s) (numC (s-exp->number s))]
+    [(s-exp-list? s)
+     (let ([sl (s-exp->list s)])
+       (case (s-exp->symbol (first sl))
+         [(+) (plusC (parse (second sl)) (parse (third sl)))]
+         [(*) (multC (parse (second sl)) (parse (third sl)))]
+         [else (error 'parse "invalid list input")]))]
+    [else (error 'parse "invalid input")]))
+
+(define (interp [a : ArithC]) : number
+  (type-case ArithC a
+    [numC (n) n]
+    [plusC (l r) (+ (interp l) (interp r))]
+    [multC (l r) (* (interp l) (interp r))]))
+
+(define l '(+ (* 1 2) (+ 3 4)))
+(interp (parse l))
+
