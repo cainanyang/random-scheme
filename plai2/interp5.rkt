6cfb323d0b48f8e52a83940c8c1171d8625e17da
cainanyang <nanyang.cai@51fanli.com>
2013-09-01 17:21:58 +0800

    add function

---
+++ plai2/interp5.rkt
@@ -0,0 +1,51 @@
+#lang plai-typed
+
+
+
+
+(define-type FunDefC
+  [fdC (name : symbol) (arg : symbol) (body : ExprC)])
+
+
+
+(define-type ExprC
+  [numC (n : number)]
+  [idC (s : symbol)]
+  [appC (fun : symbol) (arg : ExprC)]
+  [plusC (l : ExprC) (r : ExprC)]
+  [multC (l : ExprC) (r : ExprC)])
+
+(define (get-fundef [n : symbol] [fds : (listof FunDefC)]) : FunDefC
+  (cond
+    [(empty? fds) (error 'get-fundef "refference to undefined function")]
+    [(cons? fds) (cond
+                   [(equal? n (fdC-name (first fds))) (first fds)]
+                   [else (get-fundef n (rest fds))])]))
+
+(define (subst [what : ExprC] [for : symbol] [in : ExprC]) : ExprC
+  (type-case ExprC in
+    [numC (n) in]
+    [idC (s) (cond
+               [(symbol=? s for) what]
+               [else in])]
+    [appC (f a) (appC f (subst what for a))]
+    [plusC (l r) (plusC (subst what for l)
+                        (subst what for l))]
+    [multC (l r) (multC (subst what for l)
+                        (subst what for l))]))
+    
+
+(define (interp [e : ExprC] [fds : (listof FunDefC)]) : number
+  (type-case ExprC e
+    [numC (n) n]
+    [idC (_) (error 'interp "shouldn't get here")]
+    [appC (f a) (local ([define fd (get-fundef f fds)])
+                  (interp (subst a 
+                                 (fdC-arg fd)
+                                 (fdC-body fd))
+                          fds))]
+    [plusC (l r) (+ (interp l fds) (interp r fds))]
+    [multC (l r) (* (interp l fds) (interp r fds))]))
+
+
+(interp (numC 1))
